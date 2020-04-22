ccf28b1b72be16dee38b251abc7a03779ac184e6
Carolina Amaral Chaves <cchaves@ciandt.com>
2020-04-22 08:39:31 -0300

    Initial Structure

---
+++ AuthorizedPeopleWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AuthorizedPeopleWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
